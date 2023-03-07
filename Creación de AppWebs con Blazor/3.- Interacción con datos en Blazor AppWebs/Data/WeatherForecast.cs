769c44c7ca2df4c83bfa74e84612b1798f71f0e4
Diego <ogeid_to@hotmail.com>
2023-03-07 14:59:15 -0700

    Continuing next lesson

---
+++ Creación de AppWebs con Blazor/3.- Interacción con datos en Blazor AppWebs/Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace _3.__Interacción_con_datos_en_Blazor_AppWebs.Data;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
---
+++ Creación de AppWebs con Blazor/3.- Interacción con datos en Blazor AppWebs/Pages/Error.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _3.__Interacción_con_datos_en_Blazor_AppWebs.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel : PageModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    private readonly ILogger<ErrorModel> _logger;
+
+    public ErrorModel(ILogger<ErrorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
+}
